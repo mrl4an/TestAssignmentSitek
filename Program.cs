415db27a6f97085eb9037341d58de89e7af7ec1d
mrl4an <127203643+mrl4an@users.noreply.github.com>
2024-10-23 13:30:29 +0300

    hotfix

--- Program.cs
+++ Program.cs
@@ -8,12 +8,11 @@ namespace TestAssignmentSitek
     {
         static async Task Main(string[] args)
         {
-            /*
+            
             GetLastDownloadFileInfo getLastDownloadFileInfo = new GetLastDownloadFileInfo();
             string LastDownloadFile = await getLastDownloadFileInfo.GetSourseLink();//получаем ссылку на сегодняшний файл
             await HttpDownloader.DownloadLauncher(LastDownloadFile);//скачиваем архив
             Unzip.ExtarctArchive(config.TempFile_Sourse, config.CurrentDirecory);//распаковываем
-            */
             FinalTextDocument.CreateTextFile(DataBaseProcessing.ObjectInfos());
         }
     }
